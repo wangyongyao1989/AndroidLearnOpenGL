9883563c42109fba2b08e2465a054487f1084876
wangyongyao1989 <wangyongyao1989@163.com>
2024-08-02 14:25:50 +0800

    使用深度测试使3D更立体

--- app/src/main/cpp/OpenglesCube3D.cpp
+++ app/src/main/cpp/OpenglesCube3D.cpp
@@ -39,7 +39,6 @@ bool OpenglesCube3D::setupGraphics(int w, int h) {
 
 
     // 1. 设置顶点属性指针
-    // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
 
@@ -78,7 +77,7 @@ bool OpenglesCube3D::setupGraphics(int w, int h) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     if (data2) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width2, height2, 0, GL_RGB, GL_UNSIGNED_BYTE,
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE,
                      data2);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
@@ -87,9 +86,6 @@ bool OpenglesCube3D::setupGraphics(int w, int h) {
     // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
     // -------------------------------------------------------------------------------------------
     glUseProgram(shaderProgram); // don't forget to activate/use the shader before setting uniforms!
-    // either set it manually like so:
-    glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
-    // or set it via the texture class
     setInt("texture1",0);
     setInt("texture2", 1);
 
@@ -108,6 +104,8 @@ void OpenglesCube3D::renderFrame() {
     //2、使用程序
     glUseProgram(gProgram);
     checkGlError("glUseProgram");
+    //开启深度测试
+    glEnable(GL_DEPTH_TEST);
 
     // create transformations
     // make sure to initialize matrix to identity matrix first
@@ -115,9 +113,8 @@ void OpenglesCube3D::renderFrame() {
     glm::mat4 view = glm::mat4(1.0f);           //观察矩阵(View Matrix)
     glm::mat4 projection = glm::mat4(1.0f);     //投影矩阵(Projection Matrix)
     double timeValue = clock() * 10 / CLOCKS_PER_SEC;
-    model = glm::rotate(model, (float) timeValue * glm::radians(50.0f),
+    model = glm::rotate(model, (float) timeValue,
                         glm::vec3(0.5f, 1.0f, 0.0f));
-//    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     projection = glm::perspective(glm::radians(45.0f), (float) screenW / (float) screenH, 0.1f,
                                   100.0f);
@@ -134,14 +131,10 @@ void OpenglesCube3D::renderFrame() {
     // be sure to activate the shader before any calls to glUniform
     glUseProgram(shaderProgram);
     glDrawArrays(GL_TRIANGLES, 0, 36);
-//    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     checkGlError("glDrawArrays");
 }
 
 bool OpenglesCube3D::setSharderPath(const char *vertexPath, const char *fragmentPath) {
-    // configure global opengl state
-    // -----------------------------
-    glEnable(GL_DEPTH_TEST);
     return getSharderPath(vertexPath, fragmentPath);
 }
 
